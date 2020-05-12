05415a11f51e54344d56b80b1e5b80fdb7282bf2
MatthewSDick <matthew.dick@outlook.com>
2020-05-12 10:46:49 -0400

    Added hash save to DB

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-// using AuthExample.NewUser;
+using AuthExample.ViewModels;
+using AuthExample.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace AuthExample.Controllers
 {
@@ -13,11 +16,41 @@ namespace AuthExample.Controllers
   [ApiController]
   public class AuthController : ControllerBase
   {
+
+    private DatabaseContext _context;
+
+    public AuthController(DatabaseContext context)
+    {
+      _context = context;
+    }
+
     // Parameters we need to accept - {fullName: "", email: "", password: ""}
     [HttpPost("signup")]
-    public async Task<ActionResult> SignUpUser()
+    public async Task<ActionResult> SignUpUser(NewUser2 newUser)
     {
-      return Ok();
+      if (newUser.Password.Length < 7)
+      {
+        return BadRequest("Password must be at least 7 characters.");
+      }
+
+      var doesUserExist = await _context.Users.AnyAsync(User => User.Email.ToLower() == newUser.Email.ToLower());
+      if (doesUserExist)
+      {
+        return BadRequest("A user already exists with that email.");
+      }
+
+      var user = new User
+      {
+        Email = newUser.Email,
+        FullName = newUser.FullName,
+      };
+
+      var hashed = new PasswordHasher<User>().HashPassword(user, newUser.Password);
+      user.HashedPassword = hashed;
+      _context.Users.Add(user);
+      await _context.SaveChangesAsync();
+
+      return Ok(user);
     }
   }
 }
---
+++ ViewModels/NewUser2.cs
@@ -0,0 +1,10 @@
+namespace AuthExample.ViewModels
+{
+  public class NewUser2
+  {
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+  }
+
+}
